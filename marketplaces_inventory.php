9bbdad4d4d6b1341f9334ff938066489366a544f
Devesh Narayan Choukiker <choukikerdevesh25@gmail.com>
2021-01-25 21:11:53 +0530

    Add files via upload

---
+++ marketplaces_inventory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class marketplaces_inventory extends Model
+{
+    use HasFactory;
+    public $timestamps =false;
+}
